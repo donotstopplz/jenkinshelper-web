1bc877d1e1b77b6664efeb95cc937b6ff2a96419
lvlifeng <lvlifeng@tuzhanai.com>
2022-05-12 10:09:02 +0800

    :-D

--- jenkins_helper.py
+++ jenkins_helper.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Author  Lv Lifeng
+# @Time    2022-05-05 02:23
+
 """
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
